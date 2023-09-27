d95bde0d74da076011e93f00010a46825a04d3a4
ClicketyClackety <callumsykes@protonmail.com>
2023-09-26 21:34:19 -0400

    Removed old serialize method

--- src/generator/files/GUnit.cs
+++ src/generator/files/GUnit.cs
@@ -489,10 +489,11 @@ public class GUnit : IGenerateableFile
         yield return $"\tpublic override string ToString() => ToString(\"G\");";
 
         yield return "\tpublic string ToString(string? format) => ToString(format, CultureInfo.CurrentCulture);";
-        
+
         yield return "\tpublic string ToString(string? format, IFormatProvider? formatProvider)";
         yield return "\t\t=> formatProvider switch {";
-        yield return $"\t\t\t_ => $\"{{Value.ToString(format ?? \"G\", formatProvider ?? CultureInfo.CurrentCulture)}} {Unit.Symbol}\",";
+        yield return
+            $"\t\t\t_ => $\"{{Value.ToString(format ?? \"G\", formatProvider ?? CultureInfo.CurrentCulture)}} {Unit.Symbol}\",";
         yield return "\t\t};";
 
         yield return string.Empty;
@@ -507,89 +508,6 @@ public class GUnit : IGenerateableFile
     {
         yield return "}";
         yield return string.Empty;
-    }
-    
-    private IEnumerable<string> GenerateSerializer()
-    {
-        yield return "using System.Text.Json;";
-        yield return "using System.Text.Json.Serialization;";
-        yield return string.Empty;
-        yield return "namespace cunit.Json";
-        yield return "{";
-        yield return $"\tpublic sealed class {Unit.Name}JsonConverter : JsonConverter<{Unit.Name}>";
-        yield return "\t{";
-        yield return $"\t\tpublic override {Unit.Name} Read(";
-        yield return "\t\t\tref Utf8JsonReader reader,";
-        yield return "\t\t\tType typeToConvert,";
-        yield return "\t\t\tJsonSerializerOptions options)";
-        yield return "\t\t{";
-        yield return "\t\t\tif (reader.TokenType == JsonTokenType.StartArray) reader.Read();";
-        yield return string.Empty;
-        
-        if (Unit.Dimensions is null)
-        {
-            yield return $"\t\t\tvar value = reader.Get{ToTitleCase(Numerics.NumberType)}();";
-            yield return $"\t\t\treturn new {Unit.Name}(value);";
-        }
-        else
-        {
-            List<string> parameters = new(Unit.Dimensions.Length);
-            for(int i = 0; i < Unit.Dimensions.Length; i++)
-            {
-                yield return $"\t\t\treader.TryGet{ToTitleCase(Numerics.NumberType)}(out {Numerics.NumberType} {Generics.GetParam(i).ToLowerInvariant()});";
-                yield return "\t\t\treader.Read();";
-                yield return string.Empty;
-                
-                parameters.Add(Generics.GetParam(i).ToLowerInvariant());
-            }
-            
-            yield return "\t\t\tif (reader.TokenType == JsonTokenType.EndArray) reader.Read();";
-            yield return string.Empty;
-            yield return $"\t\t\treturn new {Unit.Name}({string.Join(", ", parameters)});";
-        }
-        yield return "\t\t}";
-        yield return string.Empty;
-        yield return "\t\tpublic override void Write(";
-        yield return "\t\t\tUtf8JsonWriter writer,";
-        yield return $"\t\t\t{Unit.Name} unit,";
-        yield return "\t\t\tJsonSerializerOptions options)";
-        yield return "\t\t{";
-        if (Unit.Dimensions is null)
-        {
-            yield return "\t\t\twriter.WriteNumberValue(unit.Value);";
-        }
-        else
-        {
-            yield return $"\t\t\twriter.WriteStartArray();";
-            for(int i = 0; i < Unit.Dimensions.Length; i++)
-            {
-                yield return $"\t\t\twriter.WriteNumberValue(unit.{Generics.GetParam(i)}.Value);";
-            }
-            yield return $"\t\t\twriter.WriteEndArray();";
-        }
-        yield return "\t\t}";
-        yield return "\t}";
-        yield return "}";
-        yield return string.Empty;
-    }
-
-    private string ToTitleCase(string value)
-    {
-        string val = value.ToLowerInvariant();
-        return $"{val[0].ToString().ToUpperInvariant()}{val[1..]}";
-    }
-
-    private string GetCalculation()
-    {
-        string formulae = Unit.Formula?.ToLowerInvariant();
-        for(int i = 0; i < Generics.Names.Length; i++)
-        {
-            var genericName = Generics.Names[i];
-            string parameterName = $"{genericName.ToUpperInvariant()}Value";
-            formulae = formulae.Replace($"<{i}>", parameterName);
-        }
-
-        return formulae.ToLowerInvariant();
     }
     
 }
